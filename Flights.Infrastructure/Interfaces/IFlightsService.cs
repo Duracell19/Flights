ccd01f592481eebf75480c56fadeae25ef0a66b8
Aliaksandra Liatun <Aliaksandra_Liatun@epam.com>
2016-12-28 17:51:59 +0300

    The FlightsService was changed

--- Flights.Infrastructure/Interfaces/IFlightsService.cs
+++ Flights.Infrastructure/Interfaces/IFlightsService.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Flights.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Flights.Infrastructure.Interfaces
 {
     public interface IFlightsService
     {
-        //Task<List<FlyInfo>> GetFlightAsync(string from, string to, string date);
-       // Task<List<FlyInfo>> ConfigurationOfFlightsAsync(string date, List<string> iatasFrom, List<string> iatasTo);
+        Task<List<FlyInfo>> GetFlightAsync(string from, string to, string date);
+        Task<List<FlyInfo>> ConfigurationOfFlightsAsync(string date, List<string> iatasFrom, List<string> iatasTo);
     }
 }
