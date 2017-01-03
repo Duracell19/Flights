7bf9e246edf9fc9d9d6f5f75b4329d20b42b5e4b
Aliaksandra Liatun <Aliaksandra_Liatun@epam.com>
2017-01-03 10:23:43 +0300

    Delete of favorite flight was added

--- Flights.Droid/Views/MainPageFavoritesView.cs
+++ Flights.Droid/Views/MainPageFavoritesView.cs
@@ -4,18 +4,24 @@ using Acr.UserDialogs;
 using MvvmCross.Droid.Views;
 using Android.Views;
 using Android.Widget;
+using System.Collections.ObjectModel;
+using Flights.Models;
+using MvvmCross.Platform;
+using MvvmCross.Plugins.File;
+using Flights.Infrastructure;
+using Newtonsoft.Json;
 
 namespace Flights.Droid.Views
 {
     [Activity(Label = "Favorites")]
     public class MainPageFavoritesView : MvxActivity
     {
-
+        public ObservableCollection<Favorite> FavoriteList { get; set; }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            
             SetContentView(Resource.Layout.MainPageFavoritesView);
 
             UserDialogs.Init(this);
@@ -34,7 +40,16 @@ namespace Flights.Droid.Views
                 {
                     case Resource.Id.pm_delete:
                         var index = e.Position;
-
+                        var _fileStore = Mvx.Resolve<IMvxFileStore>();
+                        string txt;
+                        if (_fileStore.TryReadTextFile(Defines.FAVORITE_LIST_FILE_NAME, out txt))
+                        {
+                            var items = JsonConvert.DeserializeObject<ObservableCollection<Favorite>>(txt);
+                            FavoriteList = items;
+                        }
+                        var listView = FindViewById<ListView>(Resource.Id.favoriteList);
+                        FavoriteList.RemoveAt(index); 
+                        _fileStore.WriteFile(Defines.FAVORITE_LIST_FILE_NAME, JsonConvert.SerializeObject(FavoriteList));
                         break;
                 }
             };
