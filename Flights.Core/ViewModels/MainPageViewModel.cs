e2d7ecd21734dc7ff43fdac2f0d7722f0833dfaf
Aliaksandra Liatun <Aliaksandra_Liatun@epam.com>
2017-01-03 15:58:24 +0300

    Comments were added

--- Flights.Core/ViewModels/MainPageViewModel.cs
+++ Flights.Core/ViewModels/MainPageViewModel.cs
@@ -7,7 +7,9 @@ namespace Flights.Core.ViewModels
     {  
         private MainPageEntryViewModel _mainPageEntryViewModel;
         private MainPageFavoritesViewModel _mainPageFavoritesViewModel;
-
+        /// <summary>
+        /// Properties witch allow to get access to MainPageEntryViewModel and MainPageFavoritesViewModel
+        /// </summary>
         public MainPageEntryViewModel MainPageEntryViewModel
         {
             get { return _mainPageEntryViewModel; }
@@ -26,7 +28,12 @@ namespace Flights.Core.ViewModels
                 RaisePropertyChanged(() => MainPageFavoritesViewModel);
             }
         }
-
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpService">Parameter to work with http service</param>
+        /// <param name="jsonConverter">Parameter to work with json converter</param>
+        /// <param name="fileStore">Parameter to work with files</param>
         public MainPageViewModel(IHttpService httpService,
             IJsonConverter jsonConverter,
             IFileStore fileStore)
